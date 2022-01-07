e595cca3811e66d315ed270f680de8db2ee328f6
G.J. Parker <gjp@entonos.com>
2022-01-07 14:19:57 -0800

    move hider declaration to ViewController

--- DIM/AppDelegate.swift
+++ DIM/AppDelegate.swift
@@ -11,23 +11,16 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     
-    //var dim = DIM()     // AppleScriptObjC bridge
-    let hider = Hider() // class is now listening for toggle
-    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        //print("in applicationDidFinishLaunching")
         NSWorkspace.shared.notificationCenter.addObserver(self, selector: #selector(self.powerOff), name: NSWorkspace.willPowerOffNotification, object: nil)
     }
     func applicationWillTerminate(_ aNotification: Notification) {
-        //print("in applicationWillTerminate")
         NotificationCenter.default.post(name: NSNotification.Name("atEnd"), object: nil)
     }
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         return true
     }
-    
     @objc func powerOff(notification: NSNotification) { // does this catch logoff, restart and shutdown?
-        //print("in powerOff")
         NSApplication.shared.terminate(self)
     }
     
